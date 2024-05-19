de166b8f6d519cc5191db23bac582d3d9b584431
Clerison <cosclerison@gmail.com>
2024-05-19 14:27:08 -0300

    :hammer:Aula 34 - Comentários e Operador Ternário

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -4,6 +4,11 @@
 
 @section('content')
 
+{{-- Comentario que não e rederizado no navegador --}}
     <h1>Essa é a Home</h1>
+
+    {{-- {{ isset($name) ? $name : 'vazio'}} --}}
+
+    {{ $teste ?? 'Valor Definido' }}
 
 @endsection
